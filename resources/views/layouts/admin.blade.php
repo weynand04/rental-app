5af3742e485ade5d556a76f801f0458b3aca805a
Weynand04 <wamaerkabor@gmail.com>
2024-04-09 20:10:58 +0700

    Update add btn  for alert message

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -57,8 +57,11 @@
                         <li class="breadcrumb-item active">Dashboard / </li>
                     </ol>
                     @if (session()->has('message'))
-                        <div class="alert alert-{{ session()->get('alert-type') }}" role="alert">
+                        <div class="alert alert-{{ session()->get('alert-type') }} alert-dismissible fade show"
+                            role="alert">
                             {{ session()->get('message') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
                         </div>
                     @endif
                     @if ($errors->any())
