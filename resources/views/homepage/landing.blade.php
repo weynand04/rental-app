ac8821754e32e2aaec1cb94346c52ae56c1d0093
Weynand04 <wamaerkabor@gmail.com>
2024-04-13 03:19:56 +0700

    Update format untuk menampilkan status

--- resources/views/homepage/landing.blade.php
+++ resources/views/homepage/landing.blade.php
@@ -2,14 +2,15 @@
 @section('content')
     <div class="row m-4" id="ads">
         @foreach ($mobil as $item)
-            <div class="col-md-6 mb-3">
+            <div class="col-md-4 mb-3">
                 <div class="app-cover p-2 shadow-md bg-white card h-100 ">
                     <div class="img-cover">
                         <img class="rounded" src="{{ Storage::url($item->gambar) }}" alt=""></img>
                     </div>
                     <div class="det mt-2 col-8">
                         <h4 class="mb-0">{{ $item->nama }}
-                            <span class="badge bg-secondary fs-8">{{ $item->status }}</span>
+                            <span
+                                class="badge {{ $item->status == 'tersedia' ? 'bg-success' : 'bg-warning' }} fs-8">{{ $item->status }}</span>
                         </h4>
                         <ul class="row my-2">
                             <li>
@@ -25,13 +26,13 @@
                                 <span>{{ $item->transmisi }}</span>
                             </li>
                         </ul>
-                        <b class="fs-8">Harga Sewa : Rp. {{ $item->harga_sewa }}</b>
+                        <b class="fs-6">Harga Sewa : Rp. {{ $item->harga_sewa }}</b>
                     </div>
-                    <div class="d-flex justify-content-around">
-                        <button class="btn btn-primary w-45 fw-semi fs-8" href="{{ route('detail', $item->slug) }}">
+                    <div class="d-flex justify-content-around mt-3">
+                        <a class="btn  btn-primary w-45 fw-semi fs-8" href="#">Sewa</a>
+                        <a class="btn btn-outline-primary w-45 fw-semi fs-8" href="{{ route('detail', $item->slug) }}">
                             Detail
-                        </button>
-                        <button class="btn btn-primary w-45 fw-semi fs-8" href="#">Sewa</button>
+                        </a>
                     </div>
                 </div>
             </div>
